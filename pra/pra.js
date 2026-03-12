d885d3b2016ccb8460926dc85fb70ae2b7996a6c
setorutocurry <ekusie0803@gmail.com>
2026-03-13 02:24:08 +0900

    Delete pra directory

--- pra/pra.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', function(){/*ハンバーガーをクリックしたら実行*/ 
-    this.classList.toggle('active');
-    document.querySelector('.slide-menu').classList.toggle('active');
-  })
